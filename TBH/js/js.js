d06afad6520041c53e18efd0b00bb0c7ee8b2232
angiezhou <shu_yu@cyberagent.co.jp>
2015-05-14 14:30:37 +0900

    TBH_20150514_02

--- TBH/js/js.js
+++ TBH/js/js.js
@@ -1,4 +1,17 @@
-$(document).ready(function(){
-	$('.carousel').carousel({
-			})
-}
+$('.carousel').carousel({
+		});
+$('.numberBox .countMinus').click(function(){
+	var count = parseInt($(this).siblings("h2").html());
+	if(count > 1){
+		$(this).siblings("h2").html(--count);
+	}
+
+	return false;
+});
+
+$('.numberBox .countPlus').click(function(){
+	var count = parseInt($(this).siblings("h2").html());
+	$(this).siblings("h2").html(++count);
+
+	return false;
+});
