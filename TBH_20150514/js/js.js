1702bd7a6bb9dd8dacfc9071eb42d478c277ca9f
angiezhou <shu_yu@cyberagent.co.jp>
2015-05-14 18:31:35 +0900

    TBH_20150514_FINAL

--- TBH_20150514/js/js.js
+++ TBH_20150514/js/js.js
@@ -1,17 +1,32 @@
-$('.carousel').carousel({
-		});
-$('.numberBox .countMinus').click(function(){
-	var count = parseInt($(this).siblings("h2").html());
-	if(count > 1){
-		$(this).siblings("h2").html(--count);
-	}
+$(document).ready(function(){
+	$('.carousel').carousel({
+	});
 
-	return false;
-});
+	$('.event-item').on({
+		mouseenter:function(e){
+			e.preventDefault();
+			var $detail=$(this).find('.detail');
+			$detail.fadeIn();
+		},
+		mouseleave:function(e){
+			e.preventDefault();
+			var $detail=$(this).find('.detail');
+			$detail.fadeOut();
+		}
+	});
+	$('.numberBox .countMinus').click(function(){
+		var count = parseInt($(this).siblings("h2").html());
+		if(count > 1){
+			$(this).siblings("h2").html(--count);
+		}
 
-$('.numberBox .countPlus').click(function(){
-	var count = parseInt($(this).siblings("h2").html());
-	$(this).siblings("h2").html(++count);
+		return false;
+	});
 
-	return false;
+	$('.numberBox .countPlus').click(function(){
+		var count = parseInt($(this).siblings("h2").html());
+		$(this).siblings("h2").html(++count);
+
+		return false;
+	});
 });
